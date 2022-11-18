c61c1e01677d61283808eec7f29a484cd0a2d888
Raghav.Garg <raghav.gargril291900@gmail.com>
2022-11-18 07:19:35 +0530

    Bubble Sort

--- Sorting/Bubble_Sort.java
+++ Sorting/Bubble_Sort.java
@@ -1,2 +1,27 @@
-package Sorting;public class Bubble_Sort {
+package Sorting;
+
+public class Bubble_Sort {
+    public static void bubbleSort(int[] a) {
+
+        for (int i=0;i<a.length-1; i++) {
+
+            for (int j=0; j<a.length-i-1; j++) {
+
+                if (a[j] > a[j+1]) {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+
+        for (int i=0; i<a.length; i++) {
+            System.out.print(a[i]+ " ");
+        }
+    }
+
+    public static void main (String args[]) {
+        int[] a = {23, 11, 4, 39, 90};
+        bubbleSort(a);
+    }
 }
