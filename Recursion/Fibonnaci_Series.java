433092a22e2b4919e123a9b7e885c28a72b66009
Raghav.Garg <raghav.gargril291900@gmail.com>
2022-11-20 07:19:46 +0530

    Fibonnaci series

--- Recursion/Fibonnaci_Series.java
+++ Recursion/Fibonnaci_Series.java
@@ -1,2 +1,20 @@
-package Recursion;public class Fibonnaci_Series {
+package Recursion;
+
+public class Fibonnaci_Series {
+    public static void fibonnaci(int a, int b, int n) {
+
+        if (n==0) {
+            return ;
+        }
+        int c = a+b;
+        System.out.println(c);
+        fibonnaci(b,c,n-1 );
+    }
+    public static void main(String[] args) {
+        int a = 0,b = 1;
+        int n =7;
+        System.out.println(a);
+        System.out.println(b);
+        fibonnaci(a,b,n-2);
+    }
 }
