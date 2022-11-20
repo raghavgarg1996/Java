a20bb99d3c79341447e36414fed6dc2c6e431f0b
Raghav.Garg <raghav.gargril291900@gmail.com>
2022-11-20 18:10:25 +0530

    Recurseive funations

---
+++ Recursion/X_to_the_powen_n.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class X_to_the_powen_n {
+
+    public  static int calcPower(int x, int n) {
+        if (x==0) {  //base case 1
+            return 0;
+        }
+
+        if (n==0) {  // base case 2
+            return 1;
+        }
+
+        int xPownm1 = calcPower(x, n-1);
+        int xPown = x * xPownm1;
+        return xPown;
+    }
+
+    public static void main(String[] args) {
+        int x=2, n=5;
+        int ans = calcPower(x,n);
+        System.out.println(ans);
+    }
+}
